357c2273450047498eed89db85e4e11d2e86c973
Vs-mydevelopmens <73613008+Vs-mydevelopmens@users.noreply.github.com>
2021-03-03 19:57:07 +0100

    new

--- js/script.js
+++ js/script.js
@@ -166,6 +166,137 @@ DynamicAdapt.prototype.arraySort = function (arr) {
 
 const da = new DynamicAdapt("max");
 da.init();
+// BildSlider
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+		}
+		if (slider.classList.contains('_gallery')) {
+			// slider.data('LightGallery').destroy(true);
+		}
+	}
+	slider_bild_callback();
+
+}
+
+function slider_bild_callback(params) { }
+
+
+
+let switchingSlider = new Swiper('.development-slider-switching__wrapper', {
+
+	// effect: 'fade',
+	// autoplay: {
+	// 	delay: 3000,
+	// 	disableOnInteraction: false,
+	// },
+
+	observer: true,
+	observeParents: true,
+	slidesPerView: 'auto',
+	spaceBetween: 15,
+	// autoHeight: true,
+	// speed: 800,
+	// touchRatio: 0,
+	// simulateTouch: false,
+	// loop: true,
+	// lazy: true,
+	preloadImages: false,
+	freeMode: true,
+	watchSlidesVisibility: true,
+	watchSlidesProgress: true,
+	// dotts
+	// pagination: {
+	// 	el: '.slider-quality__pagging',
+	// 	clickable: true,
+	// },
+	// Arrows
+	// navigation: {
+	// 	nextEl: '.header-slider-button__next',
+	// 	prevEl: '.header-slider-button__prev',
+	// },
+
+	// on: {
+	// 	lazyImageReady: function () {
+	// 		ibg();
+	// 	},
+	// },
+	// And if we need scroLLbar
+	// Scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// },
+	// breakpoints: {
+	// 	768: {
+	// 		slidesPerView: 5,
+	// 		spaceBetween: 50,
+	// 	},
+	// 	1268: {
+	// 		slidesPerView: 5,
+	// 		spaceBetween: 50,
+	// 	},
+	// },
+});
+// ==========================================
+let developmentSlider = new Swiper('.development-slider__wrapper', {
+
+	// effect: 'fade',
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+
+	observer: true,
+	observeParents: true,
+	slidesPerView: 1,
+	spaceBetween: 135,
+	// autoHeight: true,
+	speed: 800,
+	// touchRatio: 0,
+	// simulateTouch: false,
+	loop: true,
+	// lazy: true,
+	preloadImages: false,
+	// dotts
+	// pagination: {
+	// 	el: '.slider-quality__pagging',
+	// 	clickable: true,
+	// },
+	// Arrows
+	// navigation: {
+	// 	nextEl: '.header-slider-button__next',
+	// 	prevEl: '.header-slider-button__prev',
+	// },
+
+	// on: {
+	// 	lazyImageReady: function () {
+	// 		ibg();
+	// 	},
+	// },
+	// And if we need scroLLbar
+	// Scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// },
+	thumbs: {
+		swiper: switchingSlider
+	}
+});
+
 
 // ==========================================
 // <2 СКРИПТА НА ИЗОБРАЖЕНИЯ, ЕСЛИ НЕ ОТОБРАЖАЕТСЯ, АКТИВНЫЙ СКРИПТ ЗАКОМЕНТИРОВАТЬ, А ЗАКОМЕНТИРОВАННЫЙ РАССКОМЕНТИРОВАТЬ>
